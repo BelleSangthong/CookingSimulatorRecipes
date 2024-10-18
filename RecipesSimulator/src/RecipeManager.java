a6dca57bd2e66fff3e3111b49388ac7f77cb3455
Belle Sangthong <belle.sangthong@iths.se>
2024-10-18 16:11:52 +0200

    Test if showRecipe for one recipe works

--- RecipesSimulator/src/RecipeManager.java
+++ RecipesSimulator/src/RecipeManager.java
@@ -1,2 +1,9 @@
 public class RecipeManager {
+    //TODO: Hanterar listan med recept och erbjuder funktionaliteten att lägga till, ta bort och visa recept
+    public String showRecipe() {
+        return "Pancake";
+    }
 }
+
+
+
--- RecipesSimulator/test/RecipeManagerTest.java
+++ RecipesSimulator/test/RecipeManagerTest.java
@@ -1,2 +1,47 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RecipeManagerTest {
+
+    @Test
+    void testShowOneRecipes() {
+        RecipeManager m = new RecipeManager();
+        assertEquals("Pancake",m.showRecipe());
+    }
+
+    @Test
+    void testAddOneRecipe() {
+        RecipeManager m = new RecipeManager();
+
+    }
+
+    @Test
+    void testRemoveRecipe() {
+        RecipeManager m = new RecipeManager();
+
+    }
+
+
+
+    /*
+
+
+        @BeforeEach
+
+        @Test
+        public void zeroBalance() {
+            Account a = new Account("Ulf");
+            assertEquals(0,a.getBalance());
+
+         }
+
+
+        @Test
+        public void balanceDeposit() {
+            Account a = new Account("Ulf");
+            a.deposit(100);
+            assertEquals(100,a.getBalance());
+        }
+        //TODO
+     */
 }
